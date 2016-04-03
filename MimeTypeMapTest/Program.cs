f53aaf080aa04ccf615dedf2dfa82f4fab9e028b
紙鈔魚 <lettucebo@hotmail.com>
2016-04-04 06:36:32 +0800

    feat: move and organize file

---
+++ MimeTypeMapTest/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MimeTypeMap.List.Test
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("txt -> " + MimeTypeMap.GetMimeType("txt"));
+            Console.WriteLine("audio/wav -> " + MimeTypeMap.GetExtension("audio/wav"));
+        }
+    }
+}
