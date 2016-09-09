fac63f5d692a674e99dd6e8aef2447c4884a4c49
紙鈔魚 <lettucebo@hotmail.com>
2016-09-09 15:09:53 +0800

    update nuget package to 1.1.0

--- MimeTypeMap.List/Properties/AssemblyInfo.cs
+++ MimeTypeMap.List/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MimeTypeMap")]
+[assembly: AssemblyTitle("MimeTypeMap.List")]
 [assembly: AssemblyDescription( "Huge dictionary of file extensions to mime types" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany( "Samuel Neff" )]
-[assembly: AssemblyProduct("MimeTypeMap")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Money")]
+[assembly: AssemblyProduct("MimeTypeMap.List")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0047e082-f9ac-4e58-8b6f-ae4464191d74")]
+[assembly: Guid("acb7793a-4de3-4019-9ce4-527a965a0836")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
