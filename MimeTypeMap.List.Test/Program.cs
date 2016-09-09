67ae7186ade068c838aacb05bfe8e3dfaf1f5981
紙鈔魚 <lettucebo@hotmail.com>
2016-09-09 14:16:44 +0800

    add more example and correct old one to show all result

--- MimeTypeMap.List.Test/Program.cs
+++ MimeTypeMap.List.Test/Program.cs
@@ -6,8 +6,35 @@
     {
         static void Main()
         {
-            Console.WriteLine("txt -> " + MimeTypeMap.GetMimeType("txt"));
-            Console.WriteLine("audio/wav -> " + MimeTypeMap.GetExtension("audio/wav"));
+            Console.WriteLine("txt -> ");
+            foreach (string mime in MimeTypeMap.GetMimeType("txt"))
+            {
+                Console.WriteLine(mime);
+            }
+
+            // demo one extension have multipule mime
+            Console.WriteLine();
+            Console.WriteLine("zip -> ");
+            foreach (string mime in MimeTypeMap.GetMimeType("zip"))
+            {
+                Console.WriteLine(mime);
+            }
+
+            // demo get extension from mime type
+            Console.WriteLine();
+            Console.WriteLine("audio/wav -> ");
+            foreach (string mime in MimeTypeMap.GetExtension("audio/wav"))
+            {
+                Console.WriteLine(mime);
+            }
+
+            // demo get extension from mime type
+            Console.WriteLine();
+            Console.WriteLine("application/octet-stream -> ");
+            foreach (string mime in MimeTypeMap.GetExtension("application/octet-stream"))
+            {
+                Console.WriteLine(mime);
+            }
         }
     }
 }
